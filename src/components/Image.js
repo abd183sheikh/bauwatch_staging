507effd3f202db3589c7e6b24a33b1b5ba3a55c9
Abdullah Noor <abd183sheikh.gmail.com>
2025-11-22 18:09:55 +0530

    initial commit

---
+++ src/components/Image.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { storyblokEditable } from "@storyblok/react";
+
+const Image = ({blok}) => {
+  return (
+    <div {...storyblokEditable(blok)}>
+      <img src={blok.image.filename} alt="hero" style={{ width: "50%", height: "50%" }} />
+      <h1>
+        {blok.title}
+      </h1>
+    </div>
+  )
+}
+
+export default Image
